17f7676e5dd495b50dbabf23dfade99d9c220274
Daria Diachkova <dariadiachkova@mail.ru>
2020-02-29 13:46:07 +0300

    added main components and questions fetching

--- app/components/Answers.jsx
+++ app/components/Answers.jsx
@@ -1,1 +1,117 @@
 import React from "react";
+import axios from "axios";
+
+const API =
+  "https://opentdb.com/api.php?amount=50&difficulty=medium&type=multiple";
+const DEFAULT_QUERY = "redux";
+
+const fetchAnswers = url => Component =>
+  class Answers extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: null,
+        isLoading: false,
+        error: null,
+
+        isAnswered: false,
+        classNames: ["", "", "", ""]
+      };
+    }
+    componentDidMount() {
+      this.setState({ isLoading: true });
+      axios
+        .get(url)
+        .then(result =>
+          this.setState({
+            data: result.data,
+            isLoading: false
+          })
+        )
+        .catch(error =>
+          this.setState({
+            error,
+            isLoading: false
+          })
+        );
+    }
+
+    checkAnswer = e => {
+      let { isAnswered } = this.props;
+
+      if (!isAnswered) {
+        let element = e.currentTarget;
+        let { correct, increaseScore } = this.props;
+        let answer = Number(element.dataset.id);
+        let updatedClassNames = this.state.classNames;
+
+        if (answer === correct) {
+          updatedClassNames[answer - 1] = "right";
+          increaseScore();
+        } else {
+          updatedClassNames[answer - 1] = "wrong";
+        }
+
+        this.setState({
+          classNames: updatedClassNames
+        });
+
+        this.props.showButton();
+      }
+    };
+
+    shouldComponentUpdate() {
+      this.setState({
+        classNames: ["", "", "", ""]
+      });
+      return true;
+    }
+
+    render() {
+      let { answers } = this.props;
+      let { classNames } = this.state;
+
+      let transition = {
+        transitionName: "example",
+        transitionEnterTimeout: 500,
+        transitionLeaveTimeout: 300
+      };
+
+      return (
+        <div id="answers">
+          <ul>
+            <li
+              onClick={this.checkAnswer}
+              className={classNames[0]}
+              data-id="1"
+            >
+              <span>A</span> <p>{answers[0]}</p>
+            </li>
+            <li
+              onClick={this.checkAnswer}
+              className={classNames[1]}
+              data-id="2"
+            >
+              <span>B</span> <p>{answers[1]}</p>
+            </li>
+            <li
+              onClick={this.checkAnswer}
+              className={classNames[2]}
+              data-id="3"
+            >
+              <span>C</span> <p>{answers[2]}</p>
+            </li>
+            <li
+              onClick={this.checkAnswer}
+              className={classNames[3]}
+              data-id="4"
+            >
+              <span>D</span> <p>{answers[3]}</p>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+  };
+
+export default Answers;
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,1 +1,125 @@
 import React from "react";
+import Answers from "Answers";
+import Popup from "Popup";
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentQuestion: 0,
+      total: data.length,
+      showButton: false,
+      questionAnswered: false,
+      score: 0,
+      displayPopup: "flex"
+    };
+  }
+
+  pushData(currentQuestion) {
+    this.setState({
+      question: data[currentQuestion].question,
+      answers: [
+        data[currentQuestion].answers[0],
+        data[currentQuestion].answers[1],
+        data[currentQuestion].answers[2],
+        data[currentQuestion].answers[3]
+      ],
+      correct: data[currentQuestion].correct_answer,
+      currentQuestion: this.state.currentQuestion + 1
+    });
+  }
+
+  componentWillMount() {
+    let { currentQuestion } = this.state;
+    this.pushData(currentQuestion);
+  }
+
+  nextQuestion = () => {
+    let { currentQuestion, total, score } = this.state;
+
+    if (currentQuestion === total) {
+      this.setState({
+        displayPopup: "flex"
+      });
+    } else {
+      this.pushData(currentQuestion);
+      this.setState({
+        showButton: false,
+        questionAnswered: false
+      });
+    }
+  };
+
+  handleShowButton = () => {
+    this.setState({
+      showButton: true,
+      questionAnswered: true
+    });
+  };
+
+  handleStartQuiz = () => {
+    this.setState({
+      displayPopup: "none",
+      currentQuestion: 1
+    });
+  };
+
+  handleIncreaseScore = () => {
+    this.setState({
+      score: this.state.score + 1
+    });
+  };
+
+  render() {
+    let {
+      currentQuestion,
+      total,
+      question,
+      answers,
+      correct_answer,
+      showButton,
+      questionAnswered,
+      displayPopup,
+      score
+    } = this.state;
+
+    return (
+      <div className="container">
+        <Popup
+          style={{ display: displayPopup }}
+          score={score}
+          total={total}
+          startQuiz={this.handleStartQuiz}
+        />
+
+        <div className="row">
+          <div className="col-lg-10 col-lg-offset-1">
+            <div id="question">
+              <h4>
+                Question {currentQuestion}/{total}
+              </h4>
+              <p>{question}</p>
+            </div>
+            <Answers
+              answers={answers}
+              correct={correct_answer}
+              showButton={this.handleShowButton}
+              isAnswered={questionAnswered}
+              increaseScore={this.handleIncreaseScore}
+            />
+            <div id="submit">
+              {showButton ? (
+                <button className="fancy-btn" onClick={this.nextQuestion}>
+                  {currentQuestion === total ? "Finish quiz" : "Next question"}
+                </button>
+              ) : null}
+            </div>
+          </div>
+        </div>
+        <footer>Hope you're having fun</footer>
+      </div>
+    );
+  }
+}
+
+export default Main;
